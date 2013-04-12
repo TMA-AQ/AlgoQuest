658dfbf9e4fafacec3c9d9909f2b7ba8521b77d9
tma <tma@de6f410c-f7d5-3442-bb8a-973c24c52ac3>
2013-04-12 13:52:57 +0000

    load base desc and list of base
    git-svn-id: http://localhost:8080/svn/AlgoQuest/trunk@105 de6f410c-f7d5-3442-bb8a-973c24c52ac3

--- AQODBC/driver/SQLCatalog.cpp
+++ AQODBC/driver/SQLCatalog.cpp
@@ -1,4 +1,7 @@
 #include "stdafx.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 #ifdef __cplusplus
 extern "C" {
@@ -25,40 +28,109 @@ SQLRETURN  SQL_API SQLTables(SQLHSTMT StatementHandle,
 
   std::string db_path = ((AqHandleConn*)((AqHandleStmt*)StatementHandle)->conn)->connection->db_path;
   std::string cfg_path = ((AqHandleConn*)((AqHandleStmt*)StatementHandle)->conn)->connection->cfg_path;
-  
-  if (conn->server != "")
-  {
-    //
-    // Send Request
-    std::string conn_str = "connect " + conn->schema + "\n";
-    std::string stmt_str = "desc\n";
-    conn->write(conn_str.c_str());
-    conn->write(stmt_str.c_str());
 
-    //
-    // Read Answer
-    aq::Connection::buffer_t buf;
-    while (!result->eos())
-    {
-      memset(buf.data(), 0, buf.size());
-      conn->read(buf);
-      result->pushResult(buf.data(), buf.size());
-    }
-
-  }
-  else
+  if (catalog_name == "%")
   {
-    if (catalog_name == "%")
+    if (conn->server == "")
     {
       result->fillBases(db_path.c_str());
     }
     else
     {
-      if (db_name == "")
-        db_name = ((AqHandleStmt*)StatementHandle)->conn->connection->schema;
-      result->loadCatalg((db_path + db_name).c_str());
-      result->fillTables();
+      //
+      // Send Request
+      std::string stmt_str = "show\n";
+      conn->write(stmt_str.c_str());
+
+      //
+      // Read Show
+      std::stringstream showData;
+      aq::Connection::buffer_t buf;
+      while (true)
+      {
+        memset(buf.data(), 0, buf.size());
+        conn->read(buf);
+        std::string s(buf.data(), buf.size());
+        showData << s;
+        if (showData.str().find("<Databases>") != std::string::npos)
+        {
+          break;
+        }
+      }
+      
+      // Parse Show
+      std::string s = showData.str();
+      std::string::size_type pos = std::string::npos;
+      if ((pos = s.find("<Databases")) != std::string::npos)
+      {
+        s = s.substr(pos);
+        showData.str("");
+        showData << s;
+        s = showData.str();
+        result->clearBases();
+
+        try
+        {
+          boost::property_tree::ptree parser;
+          boost::property_tree::xml_parser::read_xml(showData, parser);
+          boost::property_tree::ptree dbs = parser.get_child("Databases");
+          std::for_each(dbs.begin(), dbs.end(), [&] (const boost::property_tree::ptree::value_type& db) {
+            std::string dbName = db.second.get<std::string>("<xmlattr>.Name");
+            result->addBases(dbName.c_str());
+          });
+        }
+        catch (const std::exception& ex)
+        {
+        }
+      }
+
+
     }
+  }
+  else
+  { 
+    if ((db_name == "") || (db_name == "%"))
+        db_name = ((AqHandleStmt*)StatementHandle)->conn->connection->schema;
+
+    if (conn->server == "")
+    {
+      result->loadCatalg((db_path + db_name).c_str());
+    }
+    else
+    {
+      //
+      // Send Request
+      std::string conn_str = "connect " + db_name + "\n";
+      std::string stmt_str = "desc\n";
+      conn->write(conn_str.c_str());
+      conn->write(stmt_str.c_str());
+
+      //
+      // Read Desc
+      size_t len = 0;
+      size_t size = 0;
+      std::string descData;
+      aq::Connection::buffer_t buf;
+      std::string::size_type pos = std::string::npos;
+      while (true)
+      {
+        memset(buf.data(), 0, buf.size());
+        len = conn->read(buf);
+        descData += std::string(buf.data(), len);
+        if ((pos = descData.find("</Database>")) != std::string::npos)
+        {
+          break;
+        }
+      }
+      if (((pos = descData.find("<Database")) != std::string::npos) && (descData.find("</Database>") != std::string::npos))
+      {
+        std::stringstream ssData;
+        descData = descData.substr(pos);
+        ssData << descData;
+        result->loadCatalg(ssData);
+      }
+    }
+    result->fillTables();
   }
 
 	return SQL_SUCCESS;
