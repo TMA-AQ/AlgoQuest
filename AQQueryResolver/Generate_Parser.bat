280d45b344dd45ce164f2c1fa3c01e18e81b967c
tma <tma@de6f410c-f7d5-3442-bb8a-973c24c52ac3>
2013-04-23 09:19:29 +0000

    git-svn-id: http://localhost:8080/svn/AlgoQuest/trunk@148 de6f410c-f7d5-3442-bb8a-973c24c52ac3

--- AQQueryResolver/Generate_Parser.bat
+++ AQQueryResolver/Generate_Parser.bat
@@ -5,4 +5,4 @@ cd %~dp0
 bison.exe -o %1 %2
 flex.exe -o%3 %4
 
-REM SQLParser\sql92_grm_tab.c SQLParser\SQL92_Grm.y SQLParser\lex.yy.c SQLParser\SQL92_Tok.l
+REM SQLParser\sql92_grm_tab.cpp SQLParser\SQL92_Grm.y SQLParser\lex.yy.cpp SQLParser\SQL92_Tok.l
