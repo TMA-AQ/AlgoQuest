a53fd439d7bbe57a530bb440fbcf9c8d9eb22c8e
tma <tma@de6f410c-f7d5-3442-bb8a-973c24c52ac3>
2013-04-23 09:56:11 +0000

    rename files
    git-svn-id: http://localhost:8080/svn/AlgoQuest/trunk@159 de6f410c-f7d5-3442-bb8a-973c24c52ac3

---
+++ AQQueryResolver/aq/RowWritter.cpp
@@ -0,0 +1,62 @@
+#include "RowProcessing.h"
+#include <iostream>
+
+using namespace aq;
+
+RowWritter::RowWritter(const std::string& filePath)
+	: firstRow(true)
+{
+	value = static_cast<char*>(malloc(128 * sizeof(char)));
+	if (filePath == "stdout")
+		pFOut = stdout;
+	else
+		pFOut = fopen( filePath.c_str(), "wt" );
+}
+
+RowWritter::~RowWritter()
+{
+	free(value);
+	fclose(pFOut);
+}
+
+int RowWritter::process(row_t& row)
+{
+	if (this->firstRow)
+	{
+		//write column names
+		for(size_t idx = 0; idx < row.size(); ++idx)
+    {
+      fputs(" ; ", pFOut);
+      if (row[idx].tableName != "")
+      {
+        fputs(row[idx].tableName.c_str(), pFOut);
+        fputs(".", pFOut);
+      }
+      fputs(row[idx].columnName.c_str(), pFOut);
+
+			//if (idx < this->columns.size())
+			//{
+			//	if(this->columns[idx]->Invisible)
+			//		continue;
+			//	fputs(" ; ", pFOut);
+			//	assert(this->columns[idx]);
+			//	fputs(this->columns[idx]->getDisplayName().c_str(), pFOut);
+			//}
+			//else
+			//{
+			//	fputs(" ; Count", pFOut);
+			//}
+		}
+		fputc('\n', pFOut);
+		this->firstRow = false;
+	}
+
+	for (row_t::const_iterator it = row.begin(); it != row.end(); ++it)
+	{
+		(*it).item->toString(value, (*it).type);
+		fputs(" ; ", pFOut);
+		fputs(value, pFOut);
+	}
+	fputc('\n', pFOut);
+	return 0;
+}
