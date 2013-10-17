d408a1a4c76a20db1033850b2baf309549c4271f
Thomas Martinez <thomas.martinez@algoquest.com>
2013-10-17 11:32:08 +0100

    Fix Bug: m_size not initialized

--- AQUtil/aq/FileMapper.cpp
+++ AQUtil/aq/FileMapper.cpp
@@ -1,5 +1,6 @@
 #include "FileMapper.h"
 #include "Logger.h"
+#include "Exceptions.h"
 #include <iostream>
 #include <stdint.h>
 
@@ -9,6 +10,12 @@ FileMapper::FileMapper(const char * _filename)
 	: m_filename(_filename)
 {
   this->m_fd = fopen(_filename, "rb");
+  if (this->m_fd != NULL)
+  {
+    fseek(this->m_fd, 0, SEEK_END);
+    this->m_size = ftell(this->m_fd);
+    fseek(this->m_fd, 0, SEEK_SET);
+  }
 }
 
 FileMapper::~FileMapper()
@@ -20,6 +27,15 @@ int FileMapper::read(void * buffer, size_t offset, size_t len)
 {
   assert(this->m_fd);
   assert(offset < std::numeric_limits<size_t>::max());
+  if (this->m_fd == NULL)
+  {
+    throw aq::generic_error(aq::generic_error::COULD_NOT_OPEN_FILE, m_filename);
+  }
+  if (offset >= std::numeric_limits<size_t>::max())
+  {
+    throw aq::generic_error(aq::generic_error::GENERIC, "bad offset of mapped file " + m_filename);
+  }
+  
   fseek(this->m_fd, static_cast<long>(offset), SEEK_SET);
   fread(buffer, 1, len, this->m_fd);
   if (feof(this->m_fd))
